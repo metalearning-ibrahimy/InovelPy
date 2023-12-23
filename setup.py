ef1d3b35f61aba4384c5e86e4776ee7e3c9eaa5b
metalearning-ibrahimy <153875503+metalearning-ibrahimy@users.noreply.github.com>
2023-12-23 08:11:42 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / 'README.md').read_text()
+
+setup(
+    name='InovelPy',
+    version='0.0.1',
+    license='GNU',
+    author='Taufiqurrahman',
+    author_email='taufiqurrahman.info@gmail.com',
+    url='https://github.com/metalearning-ibrahimy/InovelPy',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+      'pymongo',
+      'joblib',
+      'sklearn',
+      'glob2',
+      'tqdm',
+      "networkx",
+      'python-louvai',
+      'numpy',
+      'pickle-mixin',
+      'scipy',
+      'pandas',
+      'multiprocess',
+      'pyyaml',
+      'spacy',
+      'scispacy',
+      'cycler',
+      'thinc',
+      'wosfile',
+      'seaborn'
+    ],
+    zip_safe=True,
+    description='A Bibliometric and Scientometric Library Powered with Artificial Intelligence Tools',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+)
